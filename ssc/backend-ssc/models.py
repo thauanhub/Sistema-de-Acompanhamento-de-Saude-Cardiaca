711ef8e30e3166f0e89753c437721e557c23eeef
Thauan <thauanrocha123@gmail.com>
2026-06-03 15:29:33 -0300

    feat: creation of models and db

---
+++ ssc/backend-ssc/models.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine, Column, Integer, String, Date, Boolean, ForeignKey
+from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils.types import ChoiceType
+
+# Conexão com o bd
+db = create_engine("sqlite:///database/ssc.db")
+
+# Base do bd
+Base = declarative_base() # Faz a tradução da classe do python para uma tabela no bd
+
+# tabelas do bd
+class User(Base):
+    __tablename__ = "users" # nome da tabela no bd
+    
+    id = Column("id", Integer, primary_key=True, autoincrement=True) # id do usuário, chave primária e auto-incrementável
+    nome = Column("nome", String(255), nullable=False) # nome do usuário, obrigatório
+    email = Column("email", String(255), nullable=False, unique=True) # email do usuário, obrigatório e único
+    celular = Column("celular", String(20), nullable=False) # celular do usuário, obrigatório
+    senha = Column("senha", String(255), nullable=False) # senha do usuário, obrigatório
+    data_nascimento = Column("data_nascimento", Date, nullable=False) # data de nascimento do usuário, obrigatório
+    sexo = Column("sexo", String(10), nullable=False) # sexo do usuário, obrigatório
+    pais = Column("pais", String(100), nullable=False) # país do usuário, obrigatório
+    ativo = Column("ativo", Boolean, default=True) # status do usuário, ativo ou inativo, padrão é ativo
+    admin = Column("admin", Boolean, default=False) # se o usuário é admin ou não, padrão é não admin
+    
+    def __init__(self, nome, email, celular, senha, data_nascimento, sexo, pais, ativo=True, admin=False):
+        self.nome = nome
+        self.email = email
+        self.celular = celular
+        self.senha = senha
+        self.data_nascimento = data_nascimento
+        self.sexo = sexo
+        self.pais = pais
+        self.ativo = ativo
+        self.admin = admin
